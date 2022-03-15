8dfe5e3d5b2689f0fcb8602b44fdec1b6197392a
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-15 00:30:44 -0300

    estilo e funcao de criar usuario adicionados

--- src/views/Register/index.jsx
+++ src/views/Register/index.jsx
@@ -1,34 +1,50 @@
 import React, { useState } from 'react'
-import { KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity } from 'react-native'
+import { KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, Image } from 'react-native'
 
 import firebase from '../../config/firebaseConfig'
 import styles from './style'
+import logo from '../../../assets/icon.png'
 
-const Login = () => {
+const Register = ({ navigation }) => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   
-  const createUserFirebase = ({ navigation }) => {
-    
+  const createUserFirebase = () => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user
+        user.updateProfile({
+          displayName: name
+        }).then(() => {
+          navigation.navigate("Login")
+        })
+      }).catch((error) => {
+        console.log(error)
+        const { code, message } = error
+      })
   }
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
     >
-      <Text>Criar conta</Text>
+      <Image style={styles.imageLogo} source={logo} />
+      <Text style={styles.title}>Criar conta</Text>
       <TextInput 
         placeholder='Nome'
         type='text'
         value={name}
         onChangeText={(text) => setName(texto)}
+        style={styles.input}
       />
       <TextInput 
         placeholder='E-mail'
         type='text'
         value={email}
         onChangeText={(text) => setEmail(texto)}
+        style={styles.input}
       />
       <TextInput
         placeholder='Senha'
@@ -36,19 +52,21 @@ const Login = () => {
         type='text'
         value={password}
         onChangeText={(text) => setPassword(text)}
+        style={styles.input}
       />
       <TouchableOpacity
         disabled={email === '' || password === '' || name === ''}
         onPress={createUserFirebase}
+        style={styles.btnRegister}
       >
-        <Text>Cadastrar</Text>
+        <Text style={styles.textBtnRegister}>Cadastrar</Text>
       </TouchableOpacity>
-      <Text>
+      <Text style={styles.textLogin}>
         Já tem conta?
-        <Text onPress={() => navigation.navigate("Register")}>Fazer login</Text>
+        <Text onPress={() => navigation.navigate("Login")} style={styles.linkLogin}> Fazer login</Text>
       </Text>
     </KeyboardAvoidingView>
   )
 }
 
-export default Login
+export default Register
