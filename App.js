ffb0d7e5bbf186b4761f6b5d7659e3cac01bc0d1
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-16 20:52:30 -0300

    rotas com drawer e verificação de usuario adicionado

--- App.js
+++ App.js
@@ -1,29 +1,63 @@
+import 'react-native-gesture-handler'
+import { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Login from './src/views/Login';
-import Register from './src/views/Register';
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import firebase from './src/config/firebaseConfig'
+import AppLoading from 'expo-app-loading'
+
+import Login from './src/views/Login'
+import Register from './src/views/Register'
+import Configuration from './src/views/Configuration'
 
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
 
 export default function App() {
+  const [isLogged, setIsLogged] = useState(false)
+  const [isChecking, setIsChecking] = useState(true);
+
+  const userIsLogged = async () => {
+    await firebase.auth().onAuthStateChanged((user) => {
+      if (user) setIsLogged(true)
+      else setIsLogged(false)
+    })
+  }
+
+  if(isChecking) {
+    return (
+      <AppLoading 
+        startAsync={() => userIsLogged()}
+        onFinish={() => setIsChecking(false)}
+        onError={console.warn}
+      />
+    )
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen 
-          name='Login'
-          component={Login}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen 
-          name='Register'
-          component={Register}
-          options={{
-            headerShown: false
-          }}
-        />
-      </Stack.Navigator>
+      {isLogged ? (
+        <Drawer.Navigator initialRouteName='Configuration'>
+          <Drawer.Screen name="Configuration" component={Configuration} />
+        </Drawer.Navigator>
+      ) : (
+        <Stack.Navigator initialRouteName='Login'>
+          <Stack.Screen 
+            name='Login'
+            component={Login}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen 
+            name='Register'
+            component={Register}
+            options={{
+              headerShown: false
+            }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
