641cf6a90c88f18469f72d4d5d7c55da9149be9a
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-16 20:50:16 -0300

    tela de configurações

---
+++ src/views/Configuration/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styles from './style'
+
+const Configuration = () => {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  )
+}
+
+export default Configuration
