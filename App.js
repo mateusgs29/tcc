c861832b00c310eee587970c91ff3454bf5fb949
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-17 00:02:47 -0300

    adicionado rotas e icones do drawer

--- App.js
+++ App.js
@@ -5,10 +5,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import firebase from './src/config/firebaseConfig'
 import AppLoading from 'expo-app-loading'
+import { FontAwesome, Foundation } from '@expo/vector-icons'; 
 
 import Login from './src/views/Login'
 import Register from './src/views/Register'
 import Configuration from './src/views/Configuration'
+import CustomDrawer from './src/components/CustomDrawer'
+import Info from './src/views/Info'
+import Games from './src/views/Games'
+import { colors } from './styleGlobal'
 
 const Stack = createNativeStackNavigator()
 const Drawer = createDrawerNavigator()
@@ -37,8 +42,43 @@ export default function App() {
   return (
     <NavigationContainer>
       {isLogged ? (
-        <Drawer.Navigator initialRouteName='Configuration'>
-          <Drawer.Screen name="Configuration" component={Configuration} />
+        <Drawer.Navigator 
+          initialRouteName='Jogos' 
+          drawerContent={(props) => <CustomDrawer {...props} />}
+          screenOptions={{
+            drawerActiveTintColor: colors.purpleSecondary,
+            drawerInactiveTintColor: colors.blackLight,
+            drawerLabelStyle: {marginLeft: -20}
+          }}
+        >
+          <Drawer.Screen 
+            name="Jogos" 
+            component={Games} 
+            options={{
+              drawerIcon: ({color}) => (
+                <FontAwesome name="gamepad" size={22} color={color} />
+              ),
+              drawerLabelStyle: {marginLeft: -23}
+            }}
+          />
+          <Drawer.Screen 
+            name="Sobre o Alzheimer" 
+            component={Info} 
+            options={{
+              drawerIcon: ({color}) => (
+                <Foundation name="info" size={24} color={color} />
+                )
+              }}
+          />
+          <Drawer.Screen 
+            name="Configurações" 
+            component={Configuration} 
+            options={{
+              drawerIcon: ({color}) => (
+                <FontAwesome name="gear" size={22} color={color} />
+              )
+            }}
+          />
         </Drawer.Navigator>
       ) : (
         <Stack.Navigator initialRouteName='Login'>
