d69639b84503abd59b3320b43d917ccd9c303177
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-17 00:03:25 -0300

    tela de informacoes

---
+++ src/views/Info/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Info = () => {
+  return (
+    <View>
+      <Text>INFO</Text>
+    </View>
+  )
+}
+
+export default Info
