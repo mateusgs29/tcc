9f605872a0ec38f03129193185ac8c7bcae169d7
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-17 00:03:38 -0300

    tela de jogos

---
+++ src/views/Games/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Games = () => {
+  return (
+    <View>
+      <Text>INFO</Text>
+    </View>
+  )
+}
+
+export default Games
