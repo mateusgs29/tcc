bb7ecdff5095114539e49dcb4d04f12c971eedaa
Mateus Gomes <mateus.gs1999@gmail.com>
2022-03-17 23:35:28 -0300

    autenticacao de auto login tirada e adicionado private routes

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import firebase from './src/config/firebaseConfig'
-import AppLoading from 'expo-app-loading'
 import { FontAwesome, Foundation } from '@expo/vector-icons'; 
 
 import Login from './src/views/Login'
@@ -18,86 +17,73 @@ import { colors } from './styleGlobal'
 const Stack = createNativeStackNavigator()
 const Drawer = createDrawerNavigator()
 
-export default function App() {
-  const [isLogged, setIsLogged] = useState(false)
-  const [isChecking, setIsChecking] = useState(true);
-
-  const userIsLogged = async () => {
-    await firebase.auth().onAuthStateChanged((user) => {
-      if (user) setIsLogged(true)
-      else setIsLogged(false)
-    })
-  }
-
-  if(isChecking) {
-    return (
-      <AppLoading 
-        startAsync={() => userIsLogged()}
-        onFinish={() => setIsChecking(false)}
-        onError={console.warn}
+const PrivateRoutes = () => {
+  return (
+    <Drawer.Navigator 
+      initialRouteName='Jogos' 
+      drawerContent={(props) => <CustomDrawer {...props} />}
+      screenOptions={{
+        drawerActiveTintColor: colors.purpleSecondary,
+        drawerInactiveTintColor: colors.blackLight,
+        drawerLabelStyle: {marginLeft: -20}
+      }}
+    >
+      <Drawer.Screen 
+        name="Jogos" 
+        component={Games} 
+        options={{
+          drawerIcon: ({color}) => (
+            <FontAwesome name="gamepad" size={22} color={color} />
+          ),
+          drawerLabelStyle: {marginLeft: -23}
+        }}
       />
-    )
-  }
+      <Drawer.Screen 
+        name="Sobre o Alzheimer" 
+        component={Info} 
+        options={{
+          drawerIcon: ({color}) => (
+            <Foundation name="info" size={24} color={color} />
+            )
+          }}
+      />
+      <Drawer.Screen 
+        name="Configurações" 
+        component={Configuration} 
+        options={{
+          drawerIcon: ({color}) => (
+            <FontAwesome name="gear" size={22} color={color} />
+          )
+        }}
+      />
+    </Drawer.Navigator>
+  )
+}
 
+export default function App() {
   return (
     <NavigationContainer>
-      {isLogged ? (
-        <Drawer.Navigator 
-          initialRouteName='Jogos' 
-          drawerContent={(props) => <CustomDrawer {...props} />}
-          screenOptions={{
-            drawerActiveTintColor: colors.purpleSecondary,
-            drawerInactiveTintColor: colors.blackLight,
-            drawerLabelStyle: {marginLeft: -20}
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen
+          name="PrivateRoutes"
+          component={PrivateRoutes}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen 
+          name='Login'
+          component={Login}
+          options={{
+            headerShown: false
           }}
-        >
-          <Drawer.Screen 
-            name="Jogos" 
-            component={Games} 
-            options={{
-              drawerIcon: ({color}) => (
-                <FontAwesome name="gamepad" size={22} color={color} />
-              ),
-              drawerLabelStyle: {marginLeft: -23}
-            }}
-          />
-          <Drawer.Screen 
-            name="Sobre o Alzheimer" 
-            component={Info} 
-            options={{
-              drawerIcon: ({color}) => (
-                <Foundation name="info" size={24} color={color} />
-                )
-              }}
-          />
-          <Drawer.Screen 
-            name="Configurações" 
-            component={Configuration} 
-            options={{
-              drawerIcon: ({color}) => (
-                <FontAwesome name="gear" size={22} color={color} />
-              )
-            }}
-          />
-        </Drawer.Navigator>
-      ) : (
-        <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen 
-            name='Login'
-            component={Login}
-            options={{
-              headerShown: false
-            }}
-          />
-          <Stack.Screen 
-            name='Register'
-            component={Register}
-            options={{
-              headerShown: false
-            }}
-          />
-        </Stack.Navigator>
-      )}
+        />
+        <Stack.Screen 
+          name='Register'
+          component={Register}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
