a1a6e1b11efc3ff3113df66df6670bfa46d8b36b
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-01 00:40:12 -0300

    links para informacoes sobre o usuario e sobre o app adicionados

--- src/views/Configuration/index.jsx
+++ src/views/Configuration/index.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useRef } from 'react'
+import { View, Text, TouchableOpacity, TextInput, KeyboardAvoidingView, Platform } from 'react-native'
 import styles from './style'
+import { Feather } from '@expo/vector-icons'
+import firebase from '../../config/firebaseConfig'
+import { Modalize } from 'react-native-modalize'
 
-const Configuration = () => {
+const Configuration = ({ navigation }) => {
+  const user = firebase.auth().currentUser;
+
+  const modalizeRef = useRef(null)
+
+  const onOpen = () => {
+    modalizeRef.current?.open('top')
+  }
+
+  const modalDeleteAccount = () => {
+    return (
+      <Modalize
+        ref={modalizeRef}
+        snapPoint={200}
+        avoidKeyboardLikeIOS
+        keyboardAvoidingBehavior={Platform.OS === "android" ? "height" : "padding"}
+        scrollViewProps={{keyboardShouldPersistTaps: "always"}}
+        alwaysOpen
+      >
+        
+      </Modalize>
+    )
+  }
+
   return (
-    <View>
-      <Text>Teste</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate("InfoUser")}>
+        <Text>Informações sobre o usuário</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("InfoApp")}>
+        <Text>Sobre o aplicativo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text>Deletar conta</Text>
+      </TouchableOpacity>
     </View>
   )
 }
