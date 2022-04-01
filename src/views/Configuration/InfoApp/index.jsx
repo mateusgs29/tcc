ec86b6682e9878f94026b6b9862bc4b7515fa6c9
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-01 00:40:44 -0300

    tela de informações sobre o app

---
+++ src/views/Configuration/InfoApp/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import firebase from '../../../config/firebaseConfig'
+
+const InfoApp = () => {
+  return (
+    <View>
+      <Text>INFO</Text>
+    </View>
+  )
+}
+
+export default InfoApp
