9fcc6e1ad5c6655619994215a08855fc64ae32a5
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-01 22:05:09 -0300

    adicionado novas cores

--- styleGlobal.js
+++ styleGlobal.js
@@ -3,5 +3,8 @@ export const colors = {
   purpleSecondary: "#9D4EDD",
   white: "#E5E5E5",
   black: "#202020",
-  blackLight: "#5B5B5B"
+  blackLight: "#5B5B5B",
+  gray: "#c5c5c5",
+  red: "#e63946",
+  green: "#19BF56"
 }
