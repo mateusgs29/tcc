391eddba0c16e5343d80d9f3b6291ac63e63ad61
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-04 23:01:47 -0300

    tela de informações do aplicativo

--- src/views/Configuration/InfoApp/index.jsx
+++ src/views/Configuration/InfoApp/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import firebase from '../../../config/firebaseConfig'
+import { View, Text, ScrollView } from 'react-native'
+import styles from './style'
 
 const InfoApp = () => {
   return (
-    <View>
-      <Text>INFO</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Informações sobre o aplicativo</Text>
+      <Text style={styles.text}>
+        Esse aplicativo foi desenvolvido por Mateus Gomes de Souza como projeto para a matéria de trabalho de conclusão de curso apresentado ao Instituto Federal de Educação, Ciência e Tecnologia Baiano - Campus Guanambi para obtenção do título de Tecnólogo em Análise e Desenvolvimento de Sistemas.
+      </Text>
+      <Text style={styles.text}>
+        Ele possui o objetivo de ajudar na prevenção da doença do Alzheimer através de jogos voltados ao fortalecimento cerebral.
+      </Text>
+    </ScrollView>
   )
 }
 
