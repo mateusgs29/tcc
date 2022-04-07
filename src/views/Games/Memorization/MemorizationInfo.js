229a42267890680a67edcb0e95c1a38fa13c256b
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-07 00:22:28 -0300

    array com as informacoes dos jogos

---
+++ src/views/Games/Memorization/MemorizationInfo.js
@@ -0,0 +1,11 @@
+import Words1 from "../../../components/games/memorization/Words1"
+
+const memorizationGamesData = [
+  {
+    name: "Palavras 01",
+    component: Words1,
+    difficult: "easy"
+  }
+]
+
+export default memorizationGamesData
