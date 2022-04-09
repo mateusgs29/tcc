b5a05f51370a3b6fc42a0b05733aac73a4786a78
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-09 15:35:38 -0300

    array das palavras usadas no jogo

---
+++ src/games/memorization/Words1/wordsData.js
@@ -0,0 +1,16 @@
+const words = [
+  "Pai",
+  "Borracha",
+  "Aquário",
+  "Gato",
+  "Caderno",
+  "Sabonete",
+  "Escova",
+  "Lápis",
+  "Cachorro",
+  "Garota",
+  "Mãe",
+  "Menino",
+]
+
+export default words
