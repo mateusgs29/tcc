29eba4197e8ef33103e7c8ad39a3a75743d30a72
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-13 23:36:16 -0300

    estilo do botao

--- src/components/CustomButton/style.js
+++ src/components/CustomButton/style.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.green
   },
   greenTextBtn: {
-    color: colors.white
+    color: "#fff"
   },
   redBtn: {
     backgroundColor: colors.red
