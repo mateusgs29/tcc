5e88d1fae6ca9df1b6dba71cfbb062a92254ad0b
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-13 23:36:31 -0300

    dados para jogo das palavras da casa

---
+++ src/games/memorization/HouseWords1/houseWordsData.js
@@ -0,0 +1,25 @@
+const houseWords = {
+  kitchen: [
+    "Garfo",
+    "Frigideira",
+    "Alho",
+    "Batedeira",
+    "Faca"
+  ],
+  bathroom: [
+    "Escova",
+    "Xampu",
+    "Sabonete",
+    "Creme dental",
+    "Secador"
+  ],
+  bedroom: [
+    "Travesseiro",
+    "Cobertor",
+    "Despertador",
+    "Cama",
+    "Camisa"
+  ]
+}
+
+export default houseWords
