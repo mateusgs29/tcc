27ab96b322a8af37d7596629cc35c875bcb2fcb4
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-13 23:33:38 -0300

    adicao do novo jogo de palavras da casa

--- src/views/Games/Memorization/MemorizationInfo.js
+++ src/views/Games/Memorization/MemorizationInfo.js
@@ -1,3 +1,4 @@
+import HouseWords1 from "../../../games/memorization/HouseWords1"
 import Words1 from "../../../games/memorization/Words1"
 
 const memorizationGamesData = [
@@ -6,8 +7,19 @@ const memorizationGamesData = [
     cod: "words1",
     component: Words1,
     difficult: "easy",
+    qtdResult: 10,
     options: {
       qtdWords: 10
+    }
+  },
+  {
+    name: "Palavras da casa 01",
+    cod: "houseWords1",
+    component: HouseWords1,
+    difficult: "medium",
+    qtdResult: 12,
+    options: {
+      qtdWordsGroup: 4,
     }
   }
 ]
