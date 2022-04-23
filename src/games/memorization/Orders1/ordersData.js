7969ea127ba2d8b87dac5cdccb6d154d8378cbfb
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-22 21:49:48 -0300

    dados do jogo de pedidos

---
+++ src/games/memorization/Orders1/ordersData.js
@@ -0,0 +1,27 @@
+const orders = [
+  "Pizza de calabresa",
+  "Estrogonofe de carne",
+  "Peixe frito com arroz",
+  "Frango grelhado com arroz",
+  "Espaguete ao frutos do mar",
+  "Risoto de abóbora",
+  "Lasanha vegetariana",
+  "Bolo de morango",
+  "Torta de atum",
+  "Mousse de limão"
+]
+
+const clients = [
+  require("./imgs/avatar01.png"),
+  require("./imgs/avatar02.png"),
+  require("./imgs/avatar03.png"),
+  require("./imgs/avatar04.png"),
+  require("./imgs/avatar05.png"),
+  require("./imgs/avatar06.png"),
+  require("./imgs/avatar07.png"),
+  require("./imgs/avatar08.png"),
+  require("./imgs/avatar09.png"),
+  require("./imgs/avatar10.png")
+]
+
+export default { orders, clients }
