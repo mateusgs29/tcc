b8ce7f5b3582fc76e004730885834fd59b6a3fef
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-27 20:48:14 -0300

    dados do alfabeto relacionado letra com imagem

---
+++ src/games/translate/Words1/alphabetData.js
@@ -0,0 +1,35 @@
+import { MaterialCommunityIcons, AntDesign, FontAwesome5 } from '@expo/vector-icons';
+
+const size = 18
+const color = "black"
+
+const alphabet = {
+  A: <MaterialCommunityIcons name="hand-peace" size={size} color={color} />,
+  B: <MaterialCommunityIcons name="hand-okay" size={size} color={color} />,
+  C: <AntDesign name="like1" size={size} color={color} />,
+  D: <AntDesign name="dislike1" size={size} color={color} />,
+  E: <MaterialCommunityIcons name="hand-pointing-left" size={size} color={color} />,
+  F: <MaterialCommunityIcons name="hand-pointing-right" size={size} color={color} />,
+  G: <MaterialCommunityIcons name="hand-pointing-up" size={size} color={color} />,
+  H: <MaterialCommunityIcons name="hand-pointing-down" size={size} color={color} />,
+  I: <FontAwesome5 name="hand-paper" size={size} color={color} />,
+  J: <MaterialCommunityIcons name="emoticon-neutral-outline" size={size} color={color} />,
+  K: <MaterialCommunityIcons name="emoticon-kiss-outline" size={size} color={color} />,
+  L: <MaterialCommunityIcons name="emoticon-cry-outline" size={size} color={color} />,
+  M: <MaterialCommunityIcons name="emoticon-cool-outline" size={size} color={color} />,
+  N: <MaterialCommunityIcons name="skull" size={size} color={color} />,
+  O: <MaterialCommunityIcons name="airplane" size={size} color={color} />,
+  P: <MaterialCommunityIcons name="white-balance-sunny" size={size} color={color} />,
+  Q: <MaterialCommunityIcons name="water" size={size} color={color} />,
+  R: <MaterialCommunityIcons name="snowflake" size={size} color={color} />,
+  S: <MaterialCommunityIcons name="moon-waning-crescent" size={size} color={color} />,
+  T: <MaterialCommunityIcons name="star" size={size} color={color} />,
+  U: <MaterialCommunityIcons name="star-four-points" size={size} color={color} />,
+  V: <FontAwesome5 name="cross" size={size} color={color} />,
+  W: <MaterialCommunityIcons name="crosshairs-gps" size={size} color={color} />,
+  X: <MaterialCommunityIcons name="sword-cross" size={size} color={color} />,
+  Y: <MaterialCommunityIcons name="alien" size={size} color={color} />,
+  Z: <MaterialCommunityIcons name="bacteria" size={size} color={color} />
+}
+
+export default alphabet
