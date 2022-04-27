426a80a38c66a84683dbefc201c0e3e1a90fb71e
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-27 20:45:53 -0300

    informacoes sobre jogos de traducao

---
+++ src/views/Games/Translate/TranslateInfo.js
@@ -0,0 +1,16 @@
+import Words1 from "../../../games/translate/Words1"
+
+const translateGamesData = [
+  {
+    name: "Palavras",
+    cod: "words1",
+    component: Words1,
+    difficult: "easy",
+    qtdResult: 10,
+    options: {
+      qtdWords: 10
+    }
+  },
+]
+
+export default translateGamesData
