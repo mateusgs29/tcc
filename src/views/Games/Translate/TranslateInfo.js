2d77890241d2f9bdfb7f8e993d926b45721eed04
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-29 00:17:06 -0300

    adicionado jogo de traducao de frases

--- src/views/Games/Translate/TranslateInfo.js
+++ src/views/Games/Translate/TranslateInfo.js
@@ -1,4 +1,5 @@
 import Words1 from "../../../games/translate/Words1"
+import Phrases1 from "../../../games/translate/Phrases1"
 
 const translateGamesData = [
   {
@@ -9,6 +10,16 @@ const translateGamesData = [
     qtdResult: 10,
     options: {
       qtdWords: 10
+    }
+  },
+  {
+    name: "Frases",
+    cod: "phrases1",
+    component: Phrases1,
+    difficult: "medium",
+    qtdResult: 2,
+    options: {
+      qtdPhrases: 2
     }
   },
 ]
