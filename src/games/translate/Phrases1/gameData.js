4cd8cf014d1dc30586a9af81f99cc923a6093399
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-29 00:18:25 -0300

    dados do jogo de traducao de frases

---
+++ src/games/translate/Phrases1/gameData.js
@@ -0,0 +1,38 @@
+import { MaterialCommunityIcons, AntDesign, FontAwesome5, Entypo, MaterialIcons } from '@expo/vector-icons';
+
+const size = 18
+const color = "black"
+
+export const alphabet = {
+  A: <MaterialCommunityIcons name="fruit-grapes-outline" size={size} color={color} />,
+  B: <MaterialCommunityIcons name="spider-thread" size={size} color={color} />,
+  C: <AntDesign name="clockcircleo" size={size} color={color} />,
+  D: <Entypo name="old-phone" size={size} color={color} />,
+  E: <MaterialCommunityIcons name="arrow-bottom-left-thick" size={size} color={color} />,
+  F: <MaterialCommunityIcons name="moon-waxing-crescent" size={size} color={color} />,
+  G: <FontAwesome5 name="kiwi-bird" size={size} color={color} />,
+  H: <FontAwesome5 name="dog" size={size} color={color} />,
+  I: <MaterialCommunityIcons name="cat" size={size} color={color} />,
+  J: <MaterialCommunityIcons name="flower-tulip-outline" size={size} color={color} />,
+  K: <MaterialIcons name="house" size={size} color={color} />,
+  L: <MaterialCommunityIcons name="content-cut" size={size} color={color} />,
+  M: <MaterialCommunityIcons name="umbrella" size={size} color={color} />,
+  N: <MaterialCommunityIcons name="bug" size={size} color={color} />,
+  O: <Entypo name="icloud" size={size} color={color} />,
+  P: <FontAwesome5 name="pencil-alt" size={size} color={color} />,
+  Q: <MaterialCommunityIcons name="ship-wheel" size={size} color={color} />,
+  R: <FontAwesome5 name="car-side" size={size} color={color} />,
+  S: <FontAwesome5 name="mountain" size={size} color={color} />,
+  T: <MaterialCommunityIcons name="heart" size={size} color={color} />,
+  U: <MaterialIcons name="live-tv" size={size} color={color} />,
+  V: <FontAwesome5 name="bone" size={size} color={color} />,
+  W: <MaterialCommunityIcons name="fish" size={size} color={color} />,
+  X: <MaterialCommunityIcons name="baguette" size={size} color={color} />,
+  Y: <MaterialCommunityIcons name="bat" size={size} color={color} />,
+  Z: <MaterialCommunityIcons name="fruit-watermelon" size={size} color={color} />
+}
+
+export const phrases = [
+  "Hoje faz exatamente quinze anos quatro meses e vinte e sete dias que perdi a memória",
+  "Seja franco doutor tenho ou não tenho razão de me esquecer que me lembro das coisas"
+]
